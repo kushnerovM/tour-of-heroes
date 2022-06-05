bc6df6a8b0434937feeca85038a03629ef92aaf4
kushnerovM <Rojers1941@yandex.by>
2022-06-05 13:49:29 +0300

    services added

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,21 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+//import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[]=[];
   selectedHero?: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
-  constructor() { }
 
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  getHeroes():void{
+    this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes);
+  }
+  
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
