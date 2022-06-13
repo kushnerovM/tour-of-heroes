7cec391801faca7cf2e25f9d126110be50b51e6c
kushnerovM <Rojers1941@yandex.by>
2022-06-13 21:48:14 +0300

    added searching functionality

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -48,6 +48,12 @@ export class HeroService {
     return this.http.delete<Hero>(url,this.httpOptions).pipe(tap(_=>this.log(`deleted hero id=${id}`)),catchError(this.handleError<Hero>('deleteHero')));
   }
 
+  searchHeroes(term: string): Observable<Hero[]>{
+    if(!term.trim()){return of([]);}
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(tap(x=>x.length?this.log(`found heroes matching ${term}`):
+    this.log(`no heroes matching ${term}`)),catchError(this.handleError<Hero[]>('SearchHeroes',[])));
+  }
+
   private log(message:string){
     this.messageService.add(`HeroService: ${message}`);
   }
