7f954acce6a46b55a2d9883de23c4c542b668eda
kushnerovM <Rojers1941@yandex.by>
2022-06-24 19:36:40 +0300

    new hero properties added

--- angular-tour-of-heroes/src/app/hero.ts
+++ angular-tour-of-heroes/src/app/hero.ts
@@ -1,4 +1,6 @@
 export interface Hero {
     id: number;
     name: string;
+    class: string;
+    level: number;
 }
--- angular-tour-of-heroes/src/app/in-memory-data.service.ts
+++ angular-tour-of-heroes/src/app/in-memory-data.service.ts
@@ -8,15 +8,15 @@ import { Hero } from './hero';
 export class InMemoryDataService implements InMemoryDbService{
   createDb() {
     const heroes = [
-      { id: 12, name: 'Dr. Nice' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr. IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 12, name: 'Dr. Nice', class: 'paladin', level: 27 },
+      { id: 13, name: 'Bombasto', class: 'mage', level: 42 },
+      { id: 14, name: 'Celeritas', class: 'warrior', level: 1 },
+      { id: 15, name: 'Magneta', class: 'priest', level: 100 },
+      { id: 16, name: 'RubberMan', class: 'hunter', level: 14 },
+      { id: 17, name: 'Dynama', class: 'paladin', level: 76 },
+      { id: 18, name: 'Dr. IQ', class: 'hunter', level: 57 },
+      { id: 19, name: 'Magma', class: 'warriror', level: 7 },
+      { id: 20, name: 'Tornado', class: 'priest', level: 97 }
     ];
     return {heroes};
   }
