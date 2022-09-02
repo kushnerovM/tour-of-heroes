820a5cf2162dd4f33724619e1b3dfca414371205
kushnerovM <Rojers1941@yandex.by>
2022-09-02 12:52:11 +0300

    hero properties binded

--- angular-tour-of-heroes/src/app/hero-form/hero-form.component.ts
+++ angular-tour-of-heroes/src/app/hero-form/hero-form.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 @Component({
   selector: 'app-hero-form',
   templateUrl: './hero-form.component.html',
   styleUrls: ['./hero-form.component.css']
 })
 export class HeroFormComponent implements OnInit {
-
+    @Input() hero: Hero= {
+      id:1,
+      name:"a",
+      class:"a",
+      level:1
+    };
   constructor() { }
 
   ngOnInit(): void {
